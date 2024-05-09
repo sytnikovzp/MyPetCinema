7804620cab1b43784c1bfa21112aa971fff72855
alex <sytnikov.zp@gmail.com>
2024-05-09 15:38:17 +0300

    Refactoring Footer component to functional

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,50 +1,47 @@
-import { Component } from 'react';
 import './Footer.css';
 
-export class Footer extends Component {
-  render() {
-    return (
-      <>
-        <div id='footer'>
-          <div className='social-net footer-border'>
-            <div className='img-footer'>
-              <i className='fa fa-location-arrow' aria-hidden='true'></i>
-            </div>
-            <div className='text-footer'>
-              <span>Адрес киностудии</span>
-              <p>
-                <a href='https://maps.app.goo.gl/c8b7NSXnsYFPatBZ9'>
-                  г. Киев ул. Закревского, 22
-                </a>
-              </p>
-            </div>
+function Footer() {
+  return (
+    <>
+      <div id='footer'>
+        <div className='social-net footer-border'>
+          <div className='img-footer'>
+            <i className='fa fa-location-arrow' aria-hidden='true'></i>
           </div>
-          <div className='social-net footer-border'>
-            <div className='img-footer'>
-              <i className='fa fa-envelope' aria-hidden='true'></i>
-            </div>
-            <div className='text-footer'>
-              <span>E-mail режисёра</span>
-              <p>
-                <a href={'mailto:' + 'faculty@film.ua'}>faculty@film.ua</a>
-              </p>
-            </div>
-          </div>
-          <div className='social-net'>
-            <div className='img-footer'>
-              <i className='fa fa-phone' aria-hidden='true'></i>
-            </div>
-            <div className='text-footer'>
-              <span>Телефон автора сайта</span>
-              <p>
-                <a href={'tel:' + '+38 044 459 46 10'}>+38 044 459 46 10</a>
-              </p>
-            </div>
+          <div className='text-footer'>
+            <span>Адрес киностудии</span>
+            <p>
+              <a href='https://maps.app.goo.gl/c8b7NSXnsYFPatBZ9'>
+                г. Киев ул. Закревского, 22
+              </a>
+            </p>
           </div>
         </div>
-      </>
-    );
-  }
+        <div className='social-net footer-border'>
+          <div className='img-footer'>
+            <i className='fa fa-envelope' aria-hidden='true'></i>
+          </div>
+          <div className='text-footer'>
+            <span>E-mail режисёра</span>
+            <p>
+              <a href={'mailto:' + 'faculty@film.ua'}>faculty@film.ua</a>
+            </p>
+          </div>
+        </div>
+        <div className='social-net'>
+          <div className='img-footer'>
+            <i className='fa fa-phone' aria-hidden='true'></i>
+          </div>
+          <div className='text-footer'>
+            <span>Телефон автора сайта</span>
+            <p>
+              <a href={'tel:' + '+38 044 459 46 10'}>+38 044 459 46 10</a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Footer;
