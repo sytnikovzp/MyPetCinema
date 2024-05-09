6bb41ffb623fe9fc43375262d1cb86ac4c2a0d53
alex <sytnikov.zp@gmail.com>
2024-05-09 15:33:56 +0300

    Header shows movies from the array

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,24 +1,39 @@
-import { Component } from 'react';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getMoviesAction } from '../../store/actions/movieActions';
 import './Header.css';
 
-export class Header extends Component {
-  render() {
-    return (
-      <>
-        <div id='header'>
-          <div className='img-header'>
-            <i className='fa fa-film' aria-hidden='true'></i>
-          </div>
-          <h2>Сегодня в прокате:</h2>
-          <ul>
-            <li>
-              <a href='#'>Сердце из стали</a>
-            </li>
-          </ul>
+function Header() {
+  const dispatch = useDispatch();
+  const movies = useSelector((state) => state.arrMovies);
+
+  useEffect(() => {
+    dispatch(getMoviesAction());
+  }, [dispatch]);
+
+  console.log(movies)
+
+  return (
+    <>
+      <div id='header'>
+        <div className='img-header'>
+          <i className='fa fa-film' aria-hidden='true'></i>
         </div>
-      </>
-    );
-  }
+        <h2>Сегодня в прокате:</h2>
+        <ul>
+          <li>
+            {movies.map((movie) => {
+              return (
+                <a href={movie.id} key={movie.id}>
+                  {movie.movieTitle}
+                </a>
+              );
+            })}
+          </li>
+        </ul>
+      </div>
+    </>
+  );
 }
 
 export default Header;
