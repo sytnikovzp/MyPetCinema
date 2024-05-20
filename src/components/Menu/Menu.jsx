49ad2079e33e0b9331a21c911589b29eadbd042f
alex <sytnikov.zp@gmail.com>
2024-05-20 14:10:44 +0300

    Add react-router-dom v5

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,29 +1,56 @@
+import {
+  BrowserRouter as Router,
+  NavLink,
+  Link,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
+import MovieList from '../Main/MovieList/MovieList'
+
 import './Menu.css';
 
 function Menu() {
   return (
-    <div id='menu'>
-      <h3>Menu</h3>
-      <nav>
-        <ul>
-          <li>
-            <a href='#'>Home</a>
-          </li>
-          <li>
-            <a href='#'>Movies</a>
-          </li>
-          <li>
-            <a href='#'>Actors</a>
-          </li>
-          <li>
-            <a href='#'>Directors</a>
-          </li>
-          <li>
-            <a href='#'>Studios</a>
-          </li>
-        </ul>
-      </nav>
-    </div>
+    <Router>
+      <div id='menu'>
+        <h3>Menu</h3>
+        <nav>
+          <ul>
+            <li>
+              <Link to='/'>Home</Link>
+            </li>
+            <li>
+              <NavLink to='/movies' activeClassName='selected'>
+                Movies
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to='/actors' activeClassName='selected'>
+                Actors
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to='/directors' activeClassName='selected'>
+                Directors
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to='/studios' activeClassName='selected'>
+                Studios
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <Switch>
+        <Route path='/' exact>
+          Home
+        </Route>
+        <Route path='/movies' component={MovieList} />
+        <Redirect path='*' to='albums' />
+      </Switch>
+    </Router>
   );
 }
 
